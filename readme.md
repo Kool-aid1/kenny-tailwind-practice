3eb054e1000e5457f39a49d246fa6230530b4b7d
dfaulring <dfaulring@gmail.com>
2022-07-20 11:02:43 -0700

    initial commit

---
+++ readme.md
@@ -0,0 +1,3 @@
+# Kenny Tailwind Practice
+
+
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    extend: {
+      colors: {
+        'light-gray': '#eeeeee',
+        white: '#ffffff',
+        black: '#000000',
+        red: '#CC0000'
+      }
+    }
+  },
+  plugins: [],
+};
