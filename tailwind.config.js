b8c9395ea57ae6d449c0a35d20c1db91460afbb4
Kenny <67345281+Kool-aid1@users.noreply.github.com>
2022-07-25 09:12:24 -0700

    created wine recommendation category

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ module.exports = {
     extend: {
       colors: {
         'light-gray': '#eeeeee',
+        'gray': '#F8F8F8',
         white: '#ffffff',
         black: '#000000',
         red: '#CC0000'
