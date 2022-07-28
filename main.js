98b926c9d9fbeba78527aa7d3b4f0fd5ef2029d0
Kenny <67345281+Kool-aid1@users.noreply.github.com>
2022-07-28 13:49:11 -0700

    created sales price configuration with discount shown

--- main.js
+++ main.js
@@ -8,62 +8,80 @@
       title: '2020 Le Bosq Vin Blanc',
       origin: 'France',
       taste: 'Lime, Green Apple, Minerals',
-      price: '$14.30'
+      price: 14.30,
+      available: true,
+      salePrice: 10.00
     },
     {
       title: '2018 George Phillips Cellars Reserve Selection No.101 Zinfandel',
       origin: 'California, United States',
       taste: 'Blueberry, Cherry, Black Pepper',
-      price: '$17.99'
+      price: 17.99,
+      available: true,
+      salePrice: null
     },
     {
       title: '2018 Sierra Trasils Old Vine Zinfandel',
       origin: 'California, United States',
       taste: 'Strawberry, Plum, Red Currant, Tobacco, Black Pepper, Oak',
-      price: '$14.99'
+      price: 14.99,
+      available: true,
+      salePrice: 12.99
     },
     {
       title: "2020 L'arca Negromamaro I.G.T.",
       origin: 'Italy',
       taste: 'Red Cherry, Red Currant, Spice',
-      price: '$17.99'
+      price: 17.99,
+      available: true,
+      salePrice: null
     },
     {
       title: "2019 Honoré de Berticot Cabernet Sauvignon",
       origin: 'France',
       taste: 'Mocha, Plum, Black Cherry',
-      price: '$17.99'
+      price: 17.99,
+      available: true,
+      salePrice: null
     },
     {
       title: '2020 St. Désir Syrah/Merlot',
       origin: 'France',
       taste: 'Red Plum, Baking Spices, Chocolate',
-      price: '$17.99'
+      price: 17.99,
+      available: true,
+      salePrice: null
     },
     {
       title: '2017 La Fea Tempranillo Reserva',
       origin: 'Spain',
       taste: 'Wild Cherry, Toasted Coconut, Baking Spice',
-      price: '$21.99'
+      price: 21.99,
+      available: true,
+      salePrice: null
     },
     {
       title: "2019 Abbazia Barbera d'Asti D.O.C.G.",
       origin: 'Italy',
       taste: 'Little Berries, Plum, Spices',
-      price: '$19.99'
+      price: 19.99,
+      available: true,
+      salePrice: null
     }
   ];
   
   function createSlide(wine) {
     return `
-      <li class="splide__slide">
+      <li class="splide__slide max-w-[46%]">
         <div class="flex flex-col px-3">
-          <div class="relative bg-gray">
+          <div class="relative bg-gray aspect-[214/253]">
             <img 
               class="
                 block
                 mx-auto
                 w-1/2
+                aspect-[61/218]
+                py-4
               "
               src="./images/image_56.png"
               alt="${wine.title}" 
@@ -81,9 +99,12 @@
           </h2>
 
           <div class="flex flex-col text-[16px] leading-[22px] md:text-[18px] h-38">
-            <span class="text-sm opacity-60 h-8">${wine.origin}</span>
+            <span class="mb-1 text-sm opacity-60 h-9">${wine.origin}</span>
             <span class="mb-1 text-sm opacity-60 h-16">${wine.taste}</span>
-            <span class="font-bold mb-1 text-xl">${wine.price}</span>
+            
+            <span class="h-[50px]">${renderPrice(wine)}</span>
+
+
             <div>
               <img 
                 class="mb-7" 
@@ -95,6 +116,7 @@
         
           <button 
             class="
+              cart-btn
               w-full 
               h-[52px]
               font-bold 
@@ -102,7 +124,8 @@
               bg-red
             "
           >
-            Add to Cart
+          ${wine.available ? 'Add to Cart' : 'Out of Stock'}
+
           </button>
         </div>
       </li>
@@ -125,7 +148,7 @@
       type: 'slide',
       mediaQuery: 'min',
       perPage: '2',
-      gap: '1rem',
+ 
       rewind: true,
       breakpoints: {
         320: {
@@ -168,7 +191,25 @@
     initSlider();
   }
 
+  function calculatePercentage(wine) {
+    const percentage = 100 - ((wine.salePrice / wine.price) * 100);
+    return percentage.toFixed(0);
+  }
 
+  function renderPrice(wine) {
+
+    const discount = calculatePercentage(wine);
+    if(wine.salePrice > 0){
+      return`<span class="font-bold mb-1 text-xl text-[#CC0000]">$${wine.salePrice.toFixed(2)} </span> 
+      <div>
+        <span class="line-through"> $${wine.price.toFixed(2)}</span>
+        <span class="font-bold text-[#CC0000]">${discount}% off</span>
+      </div>`
+    } else {
+      return `<span class="font-bold mb-1 text-xl">$${wine.price.toFixed(2)}</span>`
+    }
+  }
+  
   document.addEventListener('DOMContentLoaded', init);
 })();
 
